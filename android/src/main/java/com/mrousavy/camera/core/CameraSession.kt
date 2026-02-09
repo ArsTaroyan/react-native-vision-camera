9a3138c9520784d122a3f9671736bdd3074b1bf3
ArsTaroyan <arsen13taroyan@gmail.com>
2026-02-09 21:49:32 +0400

    Revert "Fix Android calibration imports"
    This reverts commit ebb8dbded084234656b42a13fad73ead150822e7.

--- android/src/main/java/com/mrousavy/camera/core/CameraSession.kt
+++ android/src/main/java/com/mrousavy/camera/core/CameraSession.kt
@@ -4,7 +4,7 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.media.AudioManager
-import android.graphics.ImageFormat
+import android.media.ImageFormat
 import android.os.Handler
 import android.os.Looper
 import android.os.SystemClock
@@ -19,7 +19,6 @@ import androidx.camera.core.Camera
 import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.core.UseCase
 import androidx.camera.lifecycle.ProcessCameraProvider
